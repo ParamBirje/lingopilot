6ae968379c6cabb713df03856fbd7208106e7b4b
Param Birje <parambirje@gmail.com>
2024-05-14 14:52:34 +0530

    Completed Problem section outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/ui/navbar";
 import Hero from "./_components/hero";
+import { Redo } from "lucide-react";
 
 export default function HomePage() {
   return (
@@ -11,7 +12,7 @@ export default function HomePage() {
       {/* Video/Screenshot placeholder */}
       <div className="h-[60vh] w-full rounded-md bg-base-200" />
 
-      <div className="flex flex-col gap-5 rounded-md bg-base-200 py-32 text-center">
+      <div className="flex flex-col gap-5 rounded-md bg-base-200 py-28 text-center">
         <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
           This is the consequence of not having a conversation partner.
         </h2>
@@ -22,8 +23,24 @@ export default function HomePage() {
         </p>
 
         <div className="mt-10 flex items-center justify-center gap-10">
-          <div>step1</div>
-          <div>step2</div>
+          <div className="flex flex-col items-center gap-5">
+            <p className="text-4xl">🙇</p>
+            <p className="font-semibold">Step1 of something you do.</p>
+          </div>
+
+          <Redo className="opacity-50" size={50} />
+
+          <div className="flex flex-col items-center gap-5">
+            <p className="text-4xl">🥱</p>
+            <p className="font-semibold">step2 of something you do.</p>
+          </div>
+
+          <Redo className="opacity-50" size={50} />
+
+          <div className="flex flex-col items-center gap-5">
+            <p className="text-4xl">😥</p>
+            <p className="font-semibold">Step3 failure.</p>
+          </div>
         </div>
       </div>
     </main>
