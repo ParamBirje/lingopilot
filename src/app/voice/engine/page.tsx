11a286453d3714016fab682002cfab6fb6d2d051
ParamBirje <parambirje@gmail.com>
2024-05-20 14:30:51 +0530

    Added mic on/off UI functionality

--- src/app/voice/engine/page.tsx
+++ src/app/voice/engine/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { Mic } from "lucide-react";
+import { Mic, MicOff } from "lucide-react";
+import { useState } from "react";
 
 export default function Page() {
+  const [isMicOn, setIsMicOn] = useState(false);
+
+  function handleMicClick() {
+    setIsMicOn((prev) => !prev);
+  }
+
   return (
     <main className="mx-auto flex w-full max-w-7xl flex-col gap-10">
       <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl">
@@ -15,8 +22,11 @@ export default function Page() {
           <div className="h-[80%] rounded-t-md bg-slate-200"></div>
 
           <div className="flex h-[20%] items-center justify-end gap-3 p-5">
-            <button className="btn btn-circle btn-outline h-fit">
-              <Mic size={25} />
+            <button
+              onClick={handleMicClick}
+              className={`btn btn-circle btn-outline h-fit ${isMicOn && "bg-neutral text-white"}`}
+            >
+              {isMicOn ? <Mic size={25} /> : <MicOff size={25} />}
             </button>
           </div>
         </div>
