336fc5c527e78dfa449e99d4d41b5574e1711f46
Param Birje <parambirje@gmail.com>
2024-11-03 14:00:38 +0530

    api: configured stack auth middleware dependency

--- apps/api/middleware/auth.py
+++ apps/api/middleware/auth.py
@@ -4,10 +4,35 @@ Handles the authentication of the user
 from Stack Auth
 """
 
+import os
 
-async def authenticate_user():
+import requests
+from fastapi import HTTPException, Request
+
+AUTH_URL = "https://api.stack-auth.com/api/v1/users/me"
+AUTH_PROJECT_ID = str(os.environ.get("STACK_PROJECT_ID"))
+AUTH_SECRET = str(os.environ.get("STACK_SECRET_SERVER_KEY"))
+
+
+async def authenticate_user(request: Request):
     """
     Authenticate the user
     """
-    print("Authenticating user")
-    return {}
+
+    headers = {
+        "x-stack-access-type": "server",
+        "x-stack-project-id": AUTH_PROJECT_ID,
+        "x-stack-secret-server-key": AUTH_SECRET,
+        "x-stack-access-token": request.headers.get("x-stack-access-token"),
+    }
+
+    response = requests.get(AUTH_URL, headers=headers, timeout=10)
+    user_id = response.json().get("id")
+    if user_id is None:
+        print("User is not authenticated")
+        raise HTTPException(
+            status_code=401,
+            detail="User is not authenticated",
+        )
+
+    print(f"User is authenticated: {user_id}")
