8b1a33ead413f8d02609f1bd9b6a7cdd13434e47
Param Birje <parambirje@gmail.com>
2024-11-03 17:46:19 +0530

    api: setup polly_client for tts

--- apps/api/util/ai.py
+++ apps/api/util/ai.py
@@ -1,8 +1,16 @@
+"""
+Description:
+Responsible for AI related operations
+"""
+
+import boto3
 from openai import OpenAI
 
-from .env_config import SAMBANOVA_API_KEY
+from .env_config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, SAMBANOVA_API_KEY
 
-client = OpenAI(
+# LLM Client
+
+llm_client = OpenAI(
     api_key=SAMBANOVA_API_KEY,
     base_url="https://api.sambanova.ai/v1",
 )
@@ -13,3 +21,12 @@ LLAMA_405 = "Meta-Llama-3.1-405B-Instruct"
 
 LLAMA_VISION_11 = "Llama-3.2-11B-Vision-Instruct"
 LLAMA_VISION_90 = "Llama-3.2-90B-Vision-Instruct"
+
+# Polly Client (TTS)
+
+polly_client = boto3.client(
+    "polly",
+    region_name="us-east-1",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
--- apps/api/util/env_config.py
+++ apps/api/util/env_config.py
@@ -16,3 +16,6 @@ SUPABASE_URL = str(os.environ.get("SUPABASE_URL"))
 SUPABASE_KEY = str(os.environ.get("SUPABASE_KEY"))
 
 SAMBANOVA_API_KEY = str(os.environ.get("SAMBANOVA_API_KEY"))
+
+AWS_ACCESS_KEY_ID = str(os.environ.get("AWS_ACCESS_KEY_ID"))
+AWS_SECRET_ACCESS_KEY = str(os.environ.get("AWS_SECRET_ACCESS_KEY"))
