93eece12d46ef09a8b43d4f335f08fc4a0e65575
Param Birje <parambirje@gmail.com>
2024-11-06 21:55:28 +0530

    api: impl creating a new session for character convo

--- apps/api/routes/modes.py
+++ apps/api/routes/modes.py
@@ -3,8 +3,12 @@ Description:
 Routes for modes
 """
 
+import random
+import uuid
+
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 from ..middleware.auth import authenticate_user
 from ..util.db import supabase
@@ -20,10 +24,31 @@ async def get_modes():
     return {}
 
 
+class CharacterConvoCreateBody(BaseModel):
+    difficulty: str
+    language: str
+
+
 @router.post("/character-convo", dependencies=[Depends(authenticate_user)])
-async def character_convo():
+async def character_convo(body: CharacterConvoCreateBody):
     """
     Creates a new session for character convo
     also picks a random character
     """
-    return {}
+    response = supabase.table("characters").select("*").execute()
+    characters = response.data
+
+    random_character = characters[random.randint(0, len(characters) - 1)]
+    session_id = str(uuid.uuid4())
+
+    # TODO: create session in db
+
+    return JSONResponse(
+        content={
+            "character": random_character,
+            "session_id": session_id,
+            "difficulty": body.difficulty,
+            "language": body.language,
+        },
+        status_code=200,
+    )
