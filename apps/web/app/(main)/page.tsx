7b4e9d7e906476e4f381d3abce89675b5ebd4869
Param Birje <parambirje@gmail.com>
2024-11-22 20:58:47 +0530

    web: updated landing page

--- apps/web/app/(main)/page.tsx
+++ apps/web/app/(main)/page.tsx
@@ -1,16 +1,17 @@
 import { Link } from "@nextui-org/link";
 import { button as buttonStyles } from "@nextui-org/theme";
 
-import { GithubIcon } from "@/components/icons";
 import { ArrowRight } from "@phosphor-icons/react/dist/ssr";
 import { Button } from "@nextui-org/button";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
+import { HeroVideoDialog } from "@/components/ui/hero-video-dialog";
+import { Particles } from "@/components/ui/particles";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 mt-16">
+    <section className="flex flex-col items-center justify-center gap-6 py-8 md:py-10 mt-16">
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>The&nbsp;</span>
         <span className={title({ color: "violet" })}>copilot&nbsp;</span>
@@ -22,28 +23,46 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex gap-3">
-        <Button
-          as={Link}
-          startContent={<ArrowRight size={20} />}
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.dashboard}
-        >
-          Get Started
-        </Button>
-        {/* <Link */}
-        {/*   isExternal */}
-        {/*   className={buttonStyles({ variant: "bordered", radius: "full" })} */}
-        {/*   href={siteConfig.links.github} */}
-        {/* > */}
-        {/*   <GithubIcon size={20} /> */}
-        {/*   GitHub */}
-        {/* </Link> */}
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex gap-3">
+          <Button
+            as={Link}
+            startContent={<ArrowRight size={20} />}
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.dashboard}
+          >
+            Get Started
+          </Button>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href="https://www.youtube.com/watch?v=7lzxWziZY9I"
+          >
+            See how it works
+          </Link>
+        </div>
+
+        <p className="text-center text-default-500 text-sm">
+          🇬🇧 English available now!
+        </p>
       </div>
+
+      <HeroVideoDialog
+        className="aspect-video w-full mt-12 border-none"
+        videoSrc="https://www.youtube.com/embed/7lzxWziZY9I?si=5mjaTBp5iQoKjVh_"
+        thumbnailSrc="https://images.pexels.com/photos/725255/pexels-photo-725255.jpeg?cs=srgb&dl=pexels-ninauhlikova-725255.jpg&fm=jpg&w=1280&h=960"
+      />
+
+      <Particles
+        className="absolute inset-0"
+        quantity={200}
+        ease={80}
+        refresh
+      />
     </section>
   );
 }
