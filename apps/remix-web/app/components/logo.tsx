17a3b38115ea56d59d602e024d318d4cbfb8d9f6
Param Birje <parambirje@gmail.com>
2024-12-02 21:40:53 +0530

    remix: updated logo

--- apps/remix-web/app/components/logo.tsx
+++ apps/remix-web/app/components/logo.tsx
@@ -1,11 +1,13 @@
 import { Link } from "@nextui-org/link";
-import { AlarmSmokeIcon } from "lucide-react";
+import { BotIcon } from "lucide-react";
 
 export default function Logo() {
   return (
     <Link color="foreground" href="/">
-      <AlarmSmokeIcon size={18} />
-      <p className="ml-2 font-bold text-inherit">VAPOR // WAVE</p>
+      <BotIcon size={18} />
+      <p className="ml-2 font-bold text-inherit">
+        <span className="text-pink-500">LINGO</span>PILOT
+      </p>
     </Link>
   );
 }
