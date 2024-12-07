84d83b46d3e8eb00564459dd48efd55e9c0d7c14
Param Birje <parambirje@gmail.com>
2024-12-07 18:39:38 +0530

    Updated hero title and subtitle

--- apps/remix-web/app/routes/_home._index.tsx
+++ apps/remix-web/app/routes/_home._index.tsx
@@ -17,12 +17,12 @@ function Index() {
     <section className="flex flex-col items-center justify-center gap-6 py-8 md:py-10 mt-16">
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>The&nbsp;</span>
-        <span className={title({ color: "violet" })}>SaaS stack&nbsp;</span>
+        <span className={title({ color: "violet" })}>conversation partner&nbsp;</span>
         <br className="hidden md:block" />
         <span className={title()}>you&apos;ve been waiting for.</span>
         <div className={subtitle({ class: "mt-6" })}>
-          A minimalistic, yet powerful SaaS boilerplate to kickstart your next
-          project with Remix, Supabase Auth, and NextUI.
+          An A.I language learning conversation partner to practice speaking whenever and whenever
+          you want!
         </div>
       </div>
 
@@ -38,20 +38,12 @@ function Index() {
           >
             Get Started
           </Button>
-          <Button
-            as={Link}
-            isExternal
-            radius="full"
-            variant="bordered"
-            href="#"
-          >
+          <Button as={Link} isExternal radius="full" variant="bordered" href="#">
             See how it works
           </Button>
         </div>
 
-        <p className="text-center text-default-500 text-sm">
-          It&apos;s totally free!
-        </p>
+        <p className="text-center text-default-500 text-sm">It&apos;s totally free!</p>
       </div>
     </section>
   );
